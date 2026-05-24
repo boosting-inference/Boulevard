c3ffb73fa0f37fee18506d014c25a207560f9c2d
Haimo Fang <1679845561@qq.com>
2026-05-24 21:58:28 +0800

    Add initial XGBoost wrapper with conformal intervals

--- src/boulevard/__init__.py
+++ src/boulevard/__init__.py
@@ -1,5 +1,9 @@
 """Boulevard: regularized boosting wrappers with uncertainty intervals."""
 
 from boulevard._version import __version__
+from boulevard.backends.xgboost import XGBRegressor
 
-__all__ = ["__version__"]
+__all__ = [
+    "__version__",
+    "XGBRegressor",
+]
